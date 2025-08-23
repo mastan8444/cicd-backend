1b601d66e690af5158842ffcbea52994e8855f24
mastan8444 <mastan8957@gmail.com>
2025-08-23 16:57:54 +0530

    message

--- src/main/java/com/cicd/controller/ArithmaticCalculator.java
+++ src/main/java/com/cicd/controller/ArithmaticCalculator.java
@@ -6,21 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
-@RequestMapping("/")
+@RequestMapping("/")   // optional, could also remove this
 @CrossOrigin("*")
-
-
 public class ArithmaticCalculator {
-	@GetMapping("add/{A}/{B}")
-	public String add(@PathVariable("A")int a,@PathVariable("B")int b) {
-		return "Addition = " + (a + b) + ", Subtraction = " + (a - b);
-	}
-	public String sub(@PathVariable("A")int a,@PathVariable("B")int b) {
-		return "subtraction =" + (a-b);
-	}
-	public String mul(@PathVariable("A")int a,@PathVariable("B")int b) {
-		return "mul =" + (a*b);
-	}
+
+    @GetMapping("add/{A}/{B}")
+    public String add(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Addition = " + (a + b);
+    }
+
+    @GetMapping("sub/{A}/{B}")
+    public String sub(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Subtraction = " + (a - b);
+    }
+
+    @GetMapping("mul/{A}/{B}")
+    public String mul(@PathVariable("A") int a, @PathVariable("B") int b) {
+        return "Multiplication = " + (a * b);
+    }
 }
